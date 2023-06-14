97311b2dab4806a8448fcad3f9d2972e1d1d0d2e
Aqib Murtaza <51707040+aqibmurtaza9@users.noreply.github.com>
2023-06-14 22:45:34 +0500

    work on authorization to get access token using sdk

--- GoogleCalendar_App/Controllers/UserController.cs
+++ GoogleCalendar_App/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using GoogleCalendar_App.Common;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Util.Store;
+using GoogleCalendar_App.Common;
 using GoogleCalendar_App.DTO;
 using GoogleCalendar_App.IService;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoogleCalendar_App.Controllers
@@ -25,27 +29,44 @@ namespace GoogleCalendar_App.Controllers
         [Route("/auth/google")]
         public IActionResult GoogleAuth()
         {
-            string Url = "https://accounts.google.com/o/oauth2/auth?scope={0}&redirect_uri={1}&response_type={2}&client_id={3}&state={4}&access_type={5}&approval_prompt={6}";
+            string scopeURL = "https://accounts.google.com/o/oauth2/auth?scope={0}&redirect_uri={1}&response_type={2}&client_id={3}&state={4}&access_type={5}&approval_prompt={6}";
 
             string state = string.Empty;
-            var clientID = string.Empty;
-            var redirectURL = string.Empty;
+            var clientID = "798723233176-5s05agfpq2p5jamqs2rglkuu444ohfef.apps.googleusercontent.com";
+            var redirectURL = "https://localhost:7272/auth/google";
             string scope = Method.urlEncodeForGoogle("https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly").Replace("%20", "+");
             string redirect_uri_encode = Method.urlEncodeForGoogle(redirectURL);
-            string response_type = "code";
+            string response_type = "token";
             state = Method.urlEncodeForGoogle(state);
-            string access_type = "offline";
+            string access_type = "online";
             string approval_prompt = "force";
 
-            var mainURL = string.Format(Url, scope, redirect_uri_encode, response_type, clientID, state, access_type, approval_prompt);
+            var mainURL = string.Format(scopeURL, scope, redirect_uri_encode, response_type, clientID, state, access_type, approval_prompt);
+            //var accessToken = _googleCalendarService.Authenticate(string.Empty, redirectURL, clientID, "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");
+            //var script = $@"<script>
+            //        var popup = window.open('{mainURL}', 'googleAuthPopup', 'width=500,height=600');
+            //        if (window.focus) {{ popup.focus(); }}
+            //        </script>";
+
+            var Scopes = new string[] { CalendarService.Scope.Calendar };
 
 
-            var script = @"<script>
-                    var popup = window.open('mainURL', 'googleAuthPopup', 'width=500,height=600');
-                    if (window.focus) { popup.focus(); }
-                    </script>";
+            var path = @"E:\\clientsecret.json";
+            UserCredential credential;
+            string filePath = Path.Combine(Environment.CurrentDirectory, "clientsecret.json");
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "E:\\";
 
-            return Content(script, "text/html");
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            return Content(string.Empty, "text/html");
         }
 
         public async Task<IActionResult> GoogleAuhtAccessToken()
--- GoogleCalendar_App/Program.cs
+++ GoogleCalendar_App/Program.cs
@@ -1,5 +1,7 @@
 using GoogleCalendar_App.IService;
 using GoogleCalendar_App.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,18 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IGoogleCalendarService, GoogleCalendarService>();
+// Add Google authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = "798723233176-5s05agfpq2p5jamqs2rglkuu444ohfef.apps.googleusercontent.com";
+    options.ClientSecret = "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";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +39,7 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
